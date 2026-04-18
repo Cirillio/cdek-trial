da9353c99d6f44c489fee981dc8bb746b1a4a87c
Cirillio <ineed2healing@gmail.com>
2026-04-19 04:38:01 +0700

    fix: add vercel proxy for http access to api

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,10 +1,5 @@
-export const API_BASE = import.meta.env.VITE_API_URL
-export const PUBLIC_URL = import.meta.env.VITE_PUBLIC_URL
+export const API_BASE = import.meta.env.PROD
+    ? "/api/v1"
+    : import.meta.env.VITE_API_URL || "http://1e14c3489fcb.vps.myjino.ru:5000/api/v1"
 
-if (!API_BASE) {
-    throw new Error("API_BASE is not defined.")
-}
-
-if (!PUBLIC_URL) {
-    throw new Error("PUBLIC_URL is not defined.")
-}
+export const PUBLIC_URL = import.meta.env.VITE_PUBLIC_URL || ""

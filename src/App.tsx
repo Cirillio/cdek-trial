cec6c127b735c9b4226345e9c42abf5702cd1cdf
Cirillio <ineed2healing@gmail.com>
2026-04-18 10:56:36 +0700

    init project and deps

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { useState } from "react"
+
+function App() {
+    return (
+        <>
+            <h1 className="grid text-5xl text-red-500">hello!</h1>
+        </>
+    )
+}
+
+export default App
